e7bebcc5873941fc370450ab7dc3c5946b79a043
Thiago Santos <thiagohillsong@hotmail.com>
2021-11-29 11:05:10 -0300

    teste alpesone v1.0

---
+++ src/components/slider-intro/Index.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+    <div class="slider-intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-2 offset-md-9">
+            <div class="form">
+              <div  class="form-title">
+                <svg id="Camada_1" class="svg-carro" data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path id="Caminho_126" data-name="Caminho 126" d="M21.45,9.26l-1.57.33L22,11.19v7.23a1.13,1.13,0,0,1-1.11,1.1H19.23a1.12,1.12,0,0,1-1.11-1.11v-.55H5.88v.55a1.12,1.12,0,0,1-1.11,1.11H3.11A1.11,1.11,0,0,1,2,18.41V11.18l2.12-1.6L2.56,9.25a.73.73,0,0,1-.46-.32A1,1,0,0,1,2,8.47.65.65,0,0,1,2.16,8a.59.59,0,0,1,.49-.2H4.77a14.19,14.19,0,0,1,1.94-2.5,3.7,3.7,0,0,1,2.62-.83h5.34a3.7,3.7,0,0,1,2.62.83,14.19,14.19,0,0,1,1.94,2.5h2.12a.59.59,0,0,1,.49.2.65.65,0,0,1,.16.45,1,1,0,0,1-.1.46A.79.79,0,0,1,21.45,9.26ZM7,14.5V14a1.66,1.66,0,0,0-1.66-1.66H2.55v.55A1.49,1.49,0,0,0,3.17,14a2.11,2.11,0,0,0,1.34.48ZM5.88,9H18.12A12.31,12.31,0,0,0,17,6.64a2.22,2.22,0,0,0-2-1H9a2.21,2.21,0,0,0-2,1A11.74,11.74,0,0,0,5.88,9ZM12,12.84c0-.15-.1-.28-.31-.39a1.26,1.26,0,0,0-.54-.16H8.61a1,1,0,0,0-.52.15.44.44,0,0,0-.26.4A1.47,1.47,0,0,0,8.4,14.1a1.87,1.87,0,0,0,1.09.4h1.66a1.13,1.13,0,0,0,.54-.16c.21-.11.31-.24.31-.39Zm2.51,1.66a1.87,1.87,0,0,0,1.09-.4,1.44,1.44,0,0,0,.57-1.26.44.44,0,0,0-.26-.4,1,1,0,0,0-.52-.15H12.85a1.26,1.26,0,0,0-.54.16c-.21.11-.31.24-.31.39V14c0,.15.1.28.31.39a1.13,1.13,0,0,0,.54.16Zm4.17-2.21A1.66,1.66,0,0,0,17,14v.55h2.47A1.17,1.17,0,0,0,20.55,14a2.06,2.06,0,0,0,.34-1.18v-.55Z"/></svg>
+                <h2>Solicitar cotação</h2>
+              </div>
+              <div class="form-container">
+                <form action="">
+                  <input type="text" name="name" id="name" placeholder="Nome">
+                  <input type="email" name="email" id="email" placeholder="E-mail">
+                  <input type="text" name="phone" id="phone" placeholder="Telefone">
+                  <select name="model" id="model">
+                    <option value="modelo" selected>Modelo</option>
+                  </select>
+                  <input type="text" name="msg" id="msg" placeholder="Mensagem">
+                  <div class="form-checkbox">
+                    <input type="checkbox" class="form-check-input" id="checkbox">
+                    <label class="form-check-label" for="checkbox">Eu aceito a <span class="span-form">Política de Privacidade</span></label>
+                  </div>
+                  <button type="submit" class="button btn-blue btn-form">Enviar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+	</div>  
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
